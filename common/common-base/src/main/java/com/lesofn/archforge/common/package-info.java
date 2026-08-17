393c4d3a352d1008f26dc421df46e928e78f9fde
sofn <lesofn@gmail.com>
2026-08-17 08:56:38 +0800

    chore(common-base): remove legacy package deprecation note

--- common/common-base/src/main/java/com/lesofn/archforge/common/package-info.java
+++ common/common-base/src/main/java/com/lesofn/archforge/common/package-info.java
@@ -1,6 +1,3 @@
-/**
- * Legacy package for {@code common-base}. New code must use {@code com.lesofn.archforge.common.base}.
- */
 @NullMarked
 @ApplicationModule(id = "common", type = ApplicationModule.Type.OPEN, allowedDependencies = {})
 package com.lesofn.archforge.common;
