a0679f9540839bf2245d2dcca59730fbce2dfe48
sofn <lesofn@gmail.com>
2026-08-17 08:39:19 +0800

    chore(common-jpa): remove legacy package deprecation note

--- common/common-jpa/src/main/java/com/lesofn/archforge/common/package-info.java
+++ common/common-jpa/src/main/java/com/lesofn/archforge/common/package-info.java
@@ -1,6 +1,3 @@
-/**
- * Legacy package for {@code common-jpa}. New code must use {@code com.lesofn.archforge.common.jpa}.
- */
 @NullMarked
 @ApplicationModule(id = "common", type = ApplicationModule.Type.OPEN, allowedDependencies = {})
 package com.lesofn.archforge.common;
